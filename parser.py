433f3d93e2bdd2c174db531e587eb0a51a1611d0
OsgoodSchlatter <eloiposeidon@gmail.com>
2021-11-19 11:53:59 +0100

    exo 1 2 4 faits

--- parser.py
+++ parser.py
@@ -3,14 +3,18 @@
 import binascii
 
 
+
 def readfile(filename):
     f = open(filename, "rb")  # bin
     b = f.read()
+    # print(b)
     output = binascii.hexlify(b)  # hexa
-    return(b)
+    # print(output)
+
+    return (b)
 
 
-readfile("A.mp")
+# readfile("A.mp")
 
 
 def move_block(filename, b_number):
@@ -18,15 +22,26 @@ def move_block(filename, b_number):
     b = b[9:]
     a = 0
     blockstart = 0
-    b_len = int.from_bytes(b[blockstart:blockstart+4], "big") + 5
+    b_len = int.from_bytes(b[blockstart:blockstart + 4], "big") + 5
     while a != b_number:
         blockstart += b_len
         a += 1
-        b_len = int.from_bytes(b[blockstart:blockstart+4], "big") + 5
-    return(b[blockstart:blockstart+b_len])
+        b_len = int.from_bytes(b[blockstart:blockstart + 4], "big") + 5
+    return (b[blockstart:blockstart + b_len])
 
 
 # move_block("A.mp", 3)
+
+
+def dec_to_bin(x):
+    return int(bin(x)[2:])
+
+
+def bin_to_ascii(code):    
+    code=code.replace("1", " ")
+    code=code.replace("0", "X")    
+    return(code)
+
 
 
 def Q1(filename, block):
@@ -35,13 +50,13 @@ def Q1(filename, block):
     b = move_block(filename, block)
     data_start = 5
     bloc_h = dict()
-    bloc_h['largeur'] = int.from_bytes(b[data_start:data_start+3], "big")
-    bloc_h['hauteur'] = int.from_bytes(b[data_start+3:data_start+7], "big")
-    bloc_h['type de pixels'] = int.from_bytes(
-        b[data_start+7:data_start+8], "big")
+    bloc_h['largeur'] = int.from_bytes(b[data_start:data_start + 3], "big")
+    bloc_h['hauteur'] = int.from_bytes(b[data_start + 3:data_start + 7], "big")
+    bloc_h['type de pixels'] = int.from_bytes(b[data_start + 7:data_start + 8],
+                                              "big")
     bloc_H.append(bloc_h)
-    # print(bloc_H)
-    return(bloc_H)
+    print(bloc_H)
+    return (bloc_H)
 
 
 # Q1("A.mp", 0)
@@ -57,3 +72,24 @@ def Q2(block):
 
 
 # Q2(Q1("A.mp", 0))
+
+
+def Q4(filename, block):
+    affichage = " "
+    bloc_d = dict()
+    bloc_affichage = move_block(filename, block)
+    # ici on laisse l'user choisir le filename et le block masi cest forcement "A.mp" et block=2
+    data_start = 4
+    bloc_d['affichage'] = bloc_affichage[data_start:]
+
+    for line in bloc_affichage[data_start:]:
+        if len(str(dec_to_bin(line))) != 8:
+            for i in range(8 - len(str(dec_to_bin(line))) % 8):
+                affichage += "0"
+
+        affichage += str(dec_to_bin(line))
+        print(bin_to_ascii(affichage))
+        affichage=" "
+
+
+Q4("A.mp", 2)
