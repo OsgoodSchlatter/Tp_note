cb4d4d6528f8c12c4311f01c50868b1631de3980
OsgoodSchlatter <eloiposeidon@gmail.com>
2021-11-27 19:10:31 +0100

    final_outcome_v1

--- parser.py
+++ parser.py
@@ -3,18 +3,26 @@
 import binascii
 
 
+import matplotlib.pyplot as plt
+
+# filename = "A.mp"
+# filename = "./other/ok/french-flag.mp"
+# filename = "./other/ok/damier.mp"
+filename = "./other/ok/french-palette.mp"
+# filename = "./other/ok/german-flag-palette.mp"
+# filename = "./bw/nok/broken-dimensions2.mp"
+# filename = "./bw/nok/missing-data.mp"
+
 
 def readfile(filename):
     f = open(filename, "rb")  # bin
     b = f.read()
-    # print(b)
     output = binascii.hexlify(b)  # hexa
-    # print(output)
-
+    print(b[8:9])
     return (b)
 
 
-# readfile("A.mp")
+readfile(filename)
 
 
 def move_block(filename, b_number):
@@ -27,60 +35,63 @@ def move_block(filename, b_number):
         blockstart += b_len
         a += 1
         b_len = int.from_bytes(b[blockstart:blockstart + 4], "big") + 5
+        # print(b[blockstart:blockstart + b_len])
     return (b[blockstart:blockstart + b_len])
 
 
-# move_block("A.mp", 3)
+# move_block(filename, 3)
 
 
 def dec_to_bin(x):
     return int(bin(x)[2:])
 
 
-def bin_to_ascii(code):    
-    code=code.replace("1", " ")
-    code=code.replace("0", "X")    
+def bin_to_ascii(code):
+    code = code.replace("1", " ")
+    code = code.replace("0", "X")
     return(code)
 
 
+#################################
 
-def Q1(filename, block):
-    bloc_H = []
 
-    b = move_block(filename, block)
+def Q1(filename, id_block):
+
+    b = move_block(filename, id_block)
     data_start = 5
-    bloc_h = dict()
-    bloc_h['largeur'] = int.from_bytes(b[data_start:data_start + 3], "big")
-    bloc_h['hauteur'] = int.from_bytes(b[data_start + 3:data_start + 7], "big")
-    bloc_h['type de pixels'] = int.from_bytes(b[data_start + 7:data_start + 8],
-                                              "big")
-    bloc_H.append(bloc_h)
-    print(bloc_H)
-    return (bloc_H)
+    bloc = dict()
+    bloc['largeur'] = int.from_bytes(b[data_start:data_start + 3], "big")
+    bloc['hauteur'] = int.from_bytes(b[data_start + 3:data_start + 7], "big")
+    bloc['type de pixels'] = int.from_bytes(b[data_start + 7:data_start + 8],
+                                            "big")
+
+    return (bloc)
 
 
-# Q1("A.mp", 0)
+Q1(filename, 0)
+
+#################################
 
 
-def Q2(block):
-    bloc_c = dict()
+def Q2(filename, bloc):
+
     data_start = 4
-    block_commentaire = move_block("A.mp", 1)
-    bloc_c['commentaires'] = block_commentaire[data_start:-1]
-    block.append(bloc_c)
-    print(block)
+    block_commentaire = move_block(filename, 1)
+    bloc['commentaires'] = block_commentaire[data_start:-1]
+
+    print(bloc)
+    return(bloc)
 
 
-# Q2(Q1("A.mp", 0))
+# Q2(filename, Q1(filename, 0))
+
+##################################
 
 
-def Q4(filename, block):
+def Q4_afficher_image(filename, block):
     affichage = " "
-    bloc_d = dict()
     bloc_affichage = move_block(filename, block)
-    # ici on laisse l'user choisir le filename et le block masi cest forcement "A.mp" et block=2
     data_start = 4
-    bloc_d['affichage'] = bloc_affichage[data_start:]
 
     for line in bloc_affichage[data_start:]:
         if len(str(dec_to_bin(line))) != 8:
@@ -89,7 +100,89 @@ def Q4(filename, block):
 
         affichage += str(dec_to_bin(line))
         print(bin_to_ascii(affichage))
-        affichage=" "
+        affichage = " "
 
 
-Q4("A.mp", 2)
+# Q4_afficher_image(filename, 2)
+
+#################################
+
+
+def Q4_afficher_image_autre(filename, block, donnees):
+    type_de_pixel = donnees['type de pixels']
+    largeur = donnees['largeur']
+    bloc_affichage = move_block(filename, block)
+    data_start = 4
+    pixel = []
+    image = []
+    image_bonne_dimension_temp = []
+    image_bonne_dimension = []
+    i = 0
+    j = 0
+    for couleur in bloc_affichage[data_start:]:
+        i += 1
+        pixel.append(couleur)
+
+        if i == type_de_pixel:
+            image.append(pixel)
+            pixel = []
+            i = 0
+
+    for elem in image:
+        j += 1
+        image_bonne_dimension_temp.append(elem)
+
+        if j == largeur:
+            image_bonne_dimension.append(image_bonne_dimension_temp)
+            j = 0
+            image_bonne_dimension_temp = []
+
+    return(image_bonne_dimension)
+
+
+# Q4_afficher_image_autre(filename, 1, Q1(filename, 0))
+
+
+# plt.imshow(Q4_afficher_image_autre(filename, 1, Q1(filename, 0)), cmap='gray',
+#            vmin=0, vmax=255)
+# plt.show()
+
+
+#########################################
+
+
+def palette(filename, id_block_P, id_block_image, donnees):
+    largeur = donnees['largeur']
+    data_start = 4
+    bloc_palette = move_block(filename, id_block_P)
+    bloc_image = move_block(filename, id_block_image)
+    palette = []
+
+    couleur = []
+    image = []
+    image_temp = []
+    i = 0
+    row = 0
+    for elem in bloc_palette[data_start:]:
+        couleur.append(elem)
+        i += 1
+        if i == 3:
+            palette.append(couleur)
+            couleur = []
+            i = 0
+    for id_couleur in bloc_image[data_start:]:
+        image_temp.append(palette[id_couleur])
+        row += 1
+        if row == largeur:
+            image.append(image_temp)
+            image_temp = []
+            row = 0
+
+    return (image)
+
+
+# palette(filename, 1, 2, Q1(filename, 0))
+
+# plt.imshow(palette(filename, 1, 2, Q1(filename, 0)),
+#            vmin=0, vmax=255)
+# plt.show()
--- parser_test.py
+++ parser_test.py
@@ -1,18 +1,79 @@
-#!/bin/python3
-import binascii
 
 
-
-from parser import readfile, move_block, Q1, Q2
-
-
-def test_readfile():
-    minipng = readfile('A.mp')
-
-    assert len(minipng) == 53
-
-# def test_Q1():
+import matplotlib
+from parser import Q1, readfile, move_block
 
 
-#     minipng=Q1('A.mp',0)
-#     assert minipng[0]['largeur']==8
+def test_number_of_blocks_0():
+    filename = "A.mp"
+
+    assert move_block(filename, 2) != b''
+    assert move_block(filename, 3) == b''
+
+
+def test_number_of_blocks_1():
+    filename = "./other/ok/damier.mp"
+
+    assert move_block(filename, 1) != b''
+    assert move_block(filename, 2) == b''
+
+
+def test_number_of_blocks_2():
+    filename = "./other/ok/french-flag.mp"
+
+    assert move_block(filename, 1) != b''
+    assert move_block(filename, 2) == b''
+
+
+def test_data_file_0():
+    filename = "A.mp"
+    bloc = Q1(filename, 0)
+
+    assert bloc['largeur'] == 8
+    assert bloc['hauteur'] == 10
+    assert bloc['type de pixels'] == 0
+
+
+def test_data_file_1():
+    filename = "./other/ok/damier.mp"
+    bloc = Q1(filename, 0)
+
+    assert bloc['largeur'] == 64
+    assert bloc['hauteur'] == 64
+    assert bloc['type de pixels'] == 1
+
+
+def test_data_file_2():
+    filename = "./other/ok/german-flag-palette.mp"
+    bloc = Q1(filename, 0)
+
+    assert bloc['largeur'] == 18
+    assert bloc['hauteur'] == 15
+    assert bloc['type de pixels'] == 2
+
+
+def test_data_file_3():
+    filename = "./other/ok/french-flag.mp"
+    bloc = Q1(filename, 0)
+
+    assert bloc['largeur'] == 6
+    assert bloc['hauteur'] == 4
+    assert bloc['type de pixels'] == 3
+
+
+def test_magic():
+    filename = "./other/ok/french-palette.mp"
+
+    assert readfile(filename)[:9] == b"Mini-PNGH"
+
+
+def test_missing_magic():
+    filename = "./bw/nok/wrong-magic.mp"
+
+    assert readfile(filename)[:9] != b"Mini-PNGH"
+
+
+def test_correct_header():
+    filename = "./bw/nok/missing-header.mp"
+    header = readfile(filename)
+    assert header[8:9] != "H"
